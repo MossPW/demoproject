9da685a4ee37ea24eba1a097ebe7657994752be6
Patipan Watjanapron <patipan.wat>
2024-12-21 16:00:23 +0700

    Create Container.

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:demoproject/calculator_page.dart';
 import 'package:demoproject/homepage.dart';
 import 'package:flutter/material.dart';
 
@@ -12,12 +13,34 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(),
-    );
+        title: 'Flutter Demo',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: Scaffold(
+          appBar: AppBar(
+            title: const Text(
+              'Flutter Demo',
+              style: TextStyle(color: Colors.white),
+            ),
+            leading: const Icon(Icons.add),
+            backgroundColor: Colors.purple,
+          ),
+          body: Container(
+              height: 100,
+              width: 100,
+              color: Colors.orange,
+              padding: const EdgeInsets.all(20),
+              margin: const EdgeInsets.all(20),
+              child: const Text('Hello')),
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {
+              print("Hello");
+            },
+            child: const Icon(Icons.phone),
+          ),
+        ));
   }
 }
